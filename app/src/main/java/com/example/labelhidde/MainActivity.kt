f99ad2243cc71a3e65cc749ddaa25e5a0c3f5906
Edwin Roberto Astudillo Tobar <2191078@my.ipleiria.pt>
2023-02-22 04:44:43 +0000

    cambios para view bilding

--- app/src/main/java/com/example/labelhidde/MainActivity.kt
+++ app/src/main/java/com/example/labelhidde/MainActivity.kt
@@ -5,25 +5,45 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import com.example.labelhidde.databinding.ActivityMainBinding
+
+
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var btnHidde: Button
-    private lateinit var textHidde: TextView
+    /*    private lateinit var btnHidde: Button
+        private lateinit var textHidde: TextView
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+
+            btnHidde = findViewById(R.id.btnHidde)
+            textHidde = findViewById(R.id.textHidde)
+
+            btnHidde.setOnClickListener {
+                if (textHidde.visibility == View.GONE) {
+                    textHidde.visibility = View.VISIBLE
+                    btnHidde.text = "Ocultar"
+                } else {
+                    textHidde.visibility = View.GONE
+                    btnHidde.text = "Mostrar"
+                }
+            }
+        } */
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        btnHidde = findViewById(R.id.btnHidde)
-        textHidde = findViewById(R.id.textHidde)
-
-        btnHidde.setOnClickListener {
-            if (textHidde.visibility == View.GONE) {
-                textHidde.visibility = View.VISIBLE
-                btnHidde.text = "Ocultar"
+        binding.btnHidde.setOnClickListener {
+            if (binding.textHidde.visibility == View.GONE) {
+                binding.textHidde.visibility = View.VISIBLE
+                binding.btnHidde.setText(R.string.ocultar)
             } else {
-                textHidde.visibility = View.GONE
-                btnHidde.text = "Mostrar"
+                binding.textHidde.visibility = View.GONE
+                binding.btnHidde.setText(R.string.mostrar)
             }
         }
     }
 }
+
